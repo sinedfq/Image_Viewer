4bb76c2543bdd67c634058eec6979feb76475c79
Denis <99001435+sinedfq@users.noreply.github.com>
2023-12-03 23:25:06 +0700

    Small update imageLabel

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -14,6 +14,7 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QFrame>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QListWidget>
@@ -36,13 +37,12 @@ public:
     QWidget *centralwidget;
     QHBoxLayout *horizontalLayout;
     QFrame *menuFrame;
-    QVBoxLayout *verticalLayout_2;
-    QTextBrowser *textBrowser;
-    QListWidget *databaseListWidget;
-    QPushButton *createDatabaseButton;
+    QGridLayout *gridLayout;
     QPushButton *openDatabaseButton;
+    QPushButton *createDatabaseButton;
+    QTextBrowser *textBrowser;
     QPushButton *deleteAlbumButton;
-    QLabel *databaseImageLabel;
+    QListWidget *databaseListWidget;
     QVBoxLayout *verticalLayout;
     QHBoxLayout *horizontalLayoutButtons;
     QPushButton *addPhotoButton;
@@ -62,7 +62,7 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(1916, 980);
-        MainWindow->setMaximumSize(QSize(1916, 16777215));
+        MainWindow->setMaximumSize(QSize(16777215, 16777215));
         MainWindow->setMouseTracking(false);
         QIcon icon;
         icon.addFile(QString::fromUtf8("../../Downloads/ic_photo_album_128_28567.ico"), QSize(), QIcon::Normal, QIcon::Off);
@@ -78,13 +78,33 @@ public:
         menuFrame = new QFrame(centralwidget);
         menuFrame->setObjectName(QString::fromUtf8("menuFrame"));
         menuFrame->setMinimumSize(QSize(0, 178));
-        verticalLayout_2 = new QVBoxLayout(menuFrame);
-        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        gridLayout = new QGridLayout(menuFrame);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        openDatabaseButton = new QPushButton(menuFrame);
+        openDatabaseButton->setObjectName(QString::fromUtf8("openDatabaseButton"));
+
+        gridLayout->addWidget(openDatabaseButton, 3, 0, 1, 1);
+
+        createDatabaseButton = new QPushButton(menuFrame);
+        createDatabaseButton->setObjectName(QString::fromUtf8("createDatabaseButton"));
+        createDatabaseButton->setMaximumSize(QSize(16777215, 16777215));
+
+        gridLayout->addWidget(createDatabaseButton, 2, 0, 1, 1);
+
         textBrowser = new QTextBrowser(menuFrame);
         textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
-        textBrowser->setMaximumSize(QSize(300, 30));
+        textBrowser->setMaximumSize(QSize(16777215, 30));
+        textBrowser->setTabChangesFocus(false);
+        textBrowser->setUndoRedoEnabled(false);
 
-        verticalLayout_2->addWidget(textBrowser);
+        gridLayout->addWidget(textBrowser, 0, 0, 1, 1);
+
+        deleteAlbumButton = new QPushButton(menuFrame);
+        deleteAlbumButton->setObjectName(QString::fromUtf8("deleteAlbumButton"));
+        deleteAlbumButton->setMinimumSize(QSize(0, 30));
+        deleteAlbumButton->setMaximumSize(QSize(16777215, 16777215));
+
+        gridLayout->addWidget(deleteAlbumButton, 4, 0, 1, 1);
 
         databaseListWidget = new QListWidget(menuFrame);
         databaseListWidget->setObjectName(QString::fromUtf8("databaseListWidget"));
@@ -93,33 +113,9 @@ public:
         sizePolicy.setVerticalStretch(0);
         sizePolicy.setHeightForWidth(databaseListWidget->sizePolicy().hasHeightForWidth());
         databaseListWidget->setSizePolicy(sizePolicy);
-        databaseListWidget->setMaximumSize(QSize(300, 16777215));
+        databaseListWidget->setMaximumSize(QSize(16777215, 16777215));
 
-        verticalLayout_2->addWidget(databaseListWidget);
-
-        createDatabaseButton = new QPushButton(menuFrame);
-        createDatabaseButton->setObjectName(QString::fromUtf8("createDatabaseButton"));
-        createDatabaseButton->setMaximumSize(QSize(300, 16777215));
-
-        verticalLayout_2->addWidget(createDatabaseButton);
-
-        openDatabaseButton = new QPushButton(menuFrame);
-        openDatabaseButton->setObjectName(QString::fromUtf8("openDatabaseButton"));
-
-        verticalLayout_2->addWidget(openDatabaseButton);
-
-        deleteAlbumButton = new QPushButton(menuFrame);
-        deleteAlbumButton->setObjectName(QString::fromUtf8("deleteAlbumButton"));
-        deleteAlbumButton->setMinimumSize(QSize(0, 30));
-        deleteAlbumButton->setMaximumSize(QSize(300, 16777215));
-
-        verticalLayout_2->addWidget(deleteAlbumButton);
-
-        databaseImageLabel = new QLabel(menuFrame);
-        databaseImageLabel->setObjectName(QString::fromUtf8("databaseImageLabel"));
-        databaseImageLabel->setScaledContents(true);
-
-        verticalLayout_2->addWidget(databaseImageLabel);
+        gridLayout->addWidget(databaseListWidget, 1, 0, 1, 1);
 
 
         horizontalLayout->addWidget(menuFrame);
@@ -154,7 +150,7 @@ public:
         listWidget = new QListWidget(centralwidget);
         listWidget->setObjectName(QString::fromUtf8("listWidget"));
         listWidget->setMinimumSize(QSize(900, 0));
-        listWidget->setMaximumSize(QSize(900, 16777215));
+        listWidget->setMaximumSize(QSize(16777215, 16777215));
         listWidget->setContextMenuPolicy(Qt::DefaultContextMenu);
 
         verticalLayout->addWidget(listWidget);
@@ -183,7 +179,7 @@ public:
         imageLabel = new QLabel(centralwidget);
         imageLabel->setObjectName(QString::fromUtf8("imageLabel"));
         imageLabel->setMinimumSize(QSize(700, 600));
-        imageLabel->setMaximumSize(QSize(700, 700));
+        imageLabel->setMaximumSize(QSize(16777215, 16777215));
         imageLabel->setMouseTracking(true);
         imageLabel->setAutoFillBackground(false);
         imageLabel->setTextFormat(Qt::PlainText);
@@ -216,15 +212,14 @@ public:
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Image Viewer", nullptr));
         action->setText(QApplication::translate("MainWindow", "\320\230\320\275\321\204\320\276", nullptr));
         action_2->setText(QApplication::translate("MainWindow", "\320\240\320\260\320\267\321\200\320\260\320\261\320\276\321\202\321\207\320\270\320\272", nullptr));
+        openDatabaseButton->setText(QApplication::translate("MainWindow", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214 \320\260\320\273\321\214\320\261\320\276\320\274", nullptr));
+        createDatabaseButton->setText(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \321\204\320\276\321\202\320\276\320\260\320\273\321\214\320\261\320\276\320\274", nullptr));
         textBrowser->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">\320\241\320\277\320\270\321\201\320\276\320\272 \320\264\320\276\321\201\321\202\321\203\320\277\320\275\321\213\321\205 \321\204\320\276\321\202\320\276\320\260\320\273\321\214\320\261\320\276\320\274\320\276\320\262</p></body></html>", nullptr));
-        createDatabaseButton->setText(QApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \321\204\320\276\321\202\320\276\320\260\320\273\321\214\320\261\320\276\320\274", nullptr));
-        openDatabaseButton->setText(QApplication::translate("MainWindow", "\320\236\321\202\320\272\321\200\321\213\321\202\321\214 \320\260\320\273\321\214\320\261\320\276\320\274", nullptr));
         deleteAlbumButton->setText(QApplication::translate("MainWindow", "\320\243\320\264\320\260\320\273\320\270\321\202\321\214 \320\260\320\273\321\214\320\261\320\276\320\274", nullptr));
-        databaseImageLabel->setText(QString());
         addPhotoButton->setText(QApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214 \321\204\320\276\321\202\320\276", nullptr));
         deletePhotoButton->setText(QApplication::translate("MainWindow", "\320\243\320\264\320\260\320\273\320\270\321\202\321\214 \321\204\320\276\321\202\320\276", nullptr));
         prevPhotoButton->setText(QApplication::translate("MainWindow", "\320\237\321\200\320\265\320\264\321\213\320\264\321\203\321\211\320\260\321\217 \321\204\320\276\321\202\320\276\320\263\321\200\320\260\321\204\320\270\321\217", nullptr));
